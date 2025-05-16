1afc6f141d8161ee9ceca8d93ed5843074419f63
liliana_toiunda <liliana.ntw@gmail.com>
2025-05-16 20:55:44 +0300

    update render-function

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,9 +4,21 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 let lightbox;
 
 export function renderGallery(images, gallery) {
-  const markup = images.map(createImageCard).join(``);
-  gallery.insertAdjacentHTML(`beforeend`, markup);
+  const markup = images.map(createImageCard).join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
   initLightbox();
+}
+
+export function clearGallery(gallery) {
+  gallery.innerHTML = '';
+}
+
+export function showLoader(loaderElement) {
+  loaderElement.classList.remove('hidden');
+}
+
+export function hideLoader(loaderElement) {
+  loaderElement.classList.add('hidden');
 }
 
 function createImageCard({
@@ -24,10 +36,10 @@ function createImageCard({
         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
       </a>
       <div class="info">
-        <p class="info-item"><b>Likes</b>${likes}</p>
-        <p class="info-item"><b>Views</b>${views}</p>
-        <p class="info-item"><b>Comments</b>${comments}</p>
-        <p class="info-item"><b>Downloads</b>${downloads}</p>
+        <p class="info-item"><b>Likes:</b> ${likes}</p>
+        <p class="info-item"><b>Views:</b> ${views}</p>
+        <p class="info-item"><b>Comments:</b> ${comments}</p>
+        <p class="info-item"><b>Downloads:</b> ${downloads}</p>
       </div>
     </li>
   `;
